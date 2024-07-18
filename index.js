21e9c32792bebe4db47e10b40ba4a6342560d5e0
jshapi4 <jshapi4@wgu.edu>
2024-07-17 20:12:22 -0400

    Exercise 3.3 completed, converts string ID to numeric ID

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express();
 
 const persons = [
   {
-    id: "1",
+    id: 1,
     name: "Arto Hellas",
     number: "040-123456",
   },
   {
-    id: "2",
+    id: 2,
     name: "Ada Lovelace",
     number: "39-44-5323523",
   },
   {
-    id: "3",
+    id: 3,
     name: "Dan Abramov",
     number: "12-43-234345",
   },
   {
-    id: "4",
+    id: 4,
     name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
@@ -42,6 +42,17 @@ app.get("/info", (request, response) => {
     <p>${date}</>
     `
   );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
