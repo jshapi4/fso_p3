f722e50f742774866c8a5c54a1db519263e8f4fa
jshapi4 <jshapi4@wgu.edu>
2024-07-17 20:36:35 -0400

    completes the POST request functionality for Exercise 3.5

--- index.js
+++ index.js
@@ -62,6 +62,32 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 10000);
+  return randomId;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const valid = body.name && body.number;
+
+  if (!valid) {
+    return response.status(400).json({
+      error: "Must have a name AND number to add to phonebook",
+    });
+  }
+
+  const person = {
+    number: body.number,
+    name: body.name,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
