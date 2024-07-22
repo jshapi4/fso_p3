1d56bcbf15f8c1d82beb855792d34964e0c8a113
jshapi4 <jshapi4@wgu.edu>
2024-07-22 11:30:44 -0400

    Exercise 3.18

--- index.js
+++ index.js
@@ -6,32 +6,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(cors());
-
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     phone: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     phone: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     phone: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     phone: "39-23-6423122",
-//   },
-// ];
-
-app.use(express.json());
 app.use(express.static("dist"));
+app.use(express.json());
 
 morgan.token("body", (request) => {
   return request.method === "POST" ? JSON.stringify(request.body) : "";
@@ -52,10 +28,6 @@ app.use(
   )
 );
 
-app.get("/", (request, response) => {
-  response.status(200).send("<h1>Hello world from Part 3, with Phonebook!<h1>");
-});
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((people) => {
     response.json(people);
@@ -65,43 +37,47 @@ app.get("/api/persons", (request, response) => {
 const date = new Date();
 
 app.get("/info", (request, response) => {
-  response.status(200).send(
-    `<p>The phonebook has info for ${persons.length} people</p>
-    <p>${date}</>
-    `
-  );
+  Person.find({}).then((people) => {
+    response.status(200).send(
+      `<p>The phonebook has info for ${people.length} people</p>
+      <p>${date}</>
+      `
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  // const id = Number(request.params.id);
-  // const person = persons.find((person) => person.id === id);
-
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const initialLength = persons.length;
-  persons = persons.filter((person) => person.id !== id);
+  // const id = Number(request.params.id);
+  // const initialLength = persons.length;
 
-  if (persons.length === initialLength) {
-    return response.status(404).json({
-      error: `No person with ID ${id} was found. No deletions were made.`,
-    });
-  }
-  console.log(persons);
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 
-  response.status(200).json({
-    message: `Person with ID: ${id} was successfully deleted`,
-  });
+  // if (persons.length === initialLength) {
+  //   return response.status(404).json({
+  //     error: `No person with ID ${id} was found. No deletions were made.`,
+  //   });
+  // // }
+  // console.log(persons);
+
+  // response.status(200).json({
+  //   message: `Person with ID: ${id} was successfully deleted`,
+  // });
 });
 
 // const generateId = () => {
@@ -158,11 +134,37 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
-
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3003;
 
