6b79a6e3eb110663a42f1bb553b89348b5c428b6
jshapi4 <jshapi4@wgu.edu>
2024-07-22 21:34:13 -0400

    up to exercise 3.19

--- index.js
+++ index.js
@@ -85,34 +85,8 @@ app.delete("/api/persons/:id", (request, response) => {
 //   return randomId;
 // };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  // const valid = body.name && body.phone;
-  // console.log(body.name, body.phone);
-
-  // if (!valid) {
-  //   return response.status(400).json({
-  //     error: "Must have a name AND number to add to phonebook",
-  //   });
-  // }
-
-  // const duplicate = persons.find((person) => person.name == body.name);
-
-  // if (duplicate) {
-  //   return response.status(400).json({
-  //     error: "That name is already in the phone book",
-  //   });
-  // }
-
-  // const person = {
-  //   id: generateId(),
-  //   name: body.name,
-  //   phone: body.phone,
-  // };
-
-  // persons = persons.concat(person);
-  // response.status(201).json(person);
 
   if (body.name === undefined || body.phone === undefined) {
     return response.status(400).json({
@@ -125,24 +99,22 @@ app.post("/api/persons", (request, response) => {
     phone: body.phone,
   });
 
-  // notes = notes.concat(note);
-  // console.log(note);
-  // response.status(201).json(note);
-
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, phone } = request.body;
 
-  const person = {
-    name: body.name,
-    phone: body.phone,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, phone },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -159,6 +131,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
